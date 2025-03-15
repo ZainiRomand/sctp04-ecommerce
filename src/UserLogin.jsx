2c1a7fd97bcf036790cebf81484d0d4e227be488
Zaini Romand <zainiromand@gmail.com>
2025-03-15 16:15:52 +0000

    fixed showMessage danger not error

--- src/UserLogin.jsx
+++ src/UserLogin.jsx
@@ -3,17 +3,17 @@ import { Formik, Form, Field, ErrorMessage, yupToFormErrors } from 'formik';
 import * as Yup from 'yup';
 import { useFlashMessage } from './FlashMessageStore';
 import axios from 'axios';
-import {useJwt} from "./UserStore";
+import { useJwt } from "./UserStore";
 import { useLocation } from 'wouter';
 
 export default function UserLogin() {
 
     // React hooks must be at the top of the component function
-    const {showMessage } = useFlashMessage();
-    const { setJwt} = useJwt();
+    const { showMessage } = useFlashMessage();
+    const { setJwt } = useJwt();
     const [, setLocation] = useLocation();
 
- 
+
 
     // initial state for Formik
     const initialValues = {
@@ -21,29 +21,33 @@ export default function UserLogin() {
         password: ''
     }
 
-    const handleSubmit = async(values, formikHelper) =>{
-        try {
-            console.log(values);
-            const response = await axios.post(
-                `${import.meta.env.VITE_API_URL}/api/users/login`, values
-            )
-            if (response.data.message !== 'User not found') {
+    const handleSubmit = async (values, formikHelper) => {
+        // try {
+        console.log(values);
+        const response = await axios.post(
+            `${import.meta.env.VITE_API_URL}/api/users/login`, values)
+            .then(response => {
                 console.log(response.data);
                 setJwt(response.data.token);
                 showMessage('Login successful', 'success');
                 formikHelper.setSubmitting(false); // <-- indicate the form has finished submission
                 setLocation("/");
-            } else {
-                showMessage('User not found', 'error');
-                formikHelper.setSubmitting(false); // <-- indicate the form has finished submission
-            }
+            })
+            .catch(e => {
+                console.error(e);
+                formikHelper.setSubmitting(false);
+                showMessage("User not found", 'danger');
+            });
 
-        } catch (e) {
-            console.error(e);
-            showMessage("Unable to login", "error");
-        }
-
-
+        //     console.log(response.data);
+        //     setJwt(response.data.token);
+        //     showMessage('Login successful', 'success');
+        //     formikHelper.setSubmitting(false); // <-- indicate the form has finished submission
+        //     setLocation("/");
+        // } catch (e) {
+        //     console.error(e);
+        //     showMessage("User not found", 'danger');
+        // }
     }
 
     const validationSchema = Yup.object({
