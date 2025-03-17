6c1ca5daed40d44626818f551c2b2071edd7dc5f
Zaini Romand <zainiromand@gmail.com>
2025-03-17 13:33:19 +0000

    Logout and redirect to login page

--- src/UserLogout.jsx
+++ src/UserLogout.jsx
@@ -1,16 +1,23 @@
 import { useJwt } from "./UserStore";
+import { useFlashMessage } from './FlashMessageStore';
+import { useLocation } from 'wouter';
 
 export default function UserLogout() {
     const { clearJwt } = useJwt();
+    const { showMessage } = useFlashMessage();
+    const [, setLocation] = useLocation();
 
     const handleLogout = () => {
-            clearJwt();
-        };
+        clearJwt();
+        setLocation("/login");
+        showMessage("You have successfully logout from E-Shop","success");
+    };
     return (
-        <div className="container mt-5">
+        <div className="container mt-5 mb-3">
             <h2>Logout</h2>
-            <h2>You have successfully logout from E-Shop</h2>
+
             <button type="submit" className="btn btn-primary" onClick={handleLogout}>Logout</button>
+
         </div>
     )
 }
