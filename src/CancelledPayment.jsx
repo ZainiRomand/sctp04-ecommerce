e1850a4534d1891ec05285ce358c91fc37e19486
ZainiRomand <141402439+ZainiRomand@users.noreply.github.com>
2025-03-25 01:53:39 +0800

    handled account logout after stripe

--- src/CancelledPayment.jsx
+++ src/CancelledPayment.jsx
@@ -1,4 +1,19 @@
+import { useAuth } from './AuthContext';
+import { useJwt } from "./UserStore";
+
 export default function CancelledPayment() {
+    const { getJwt } = useJwt();
+    const { isLoggedIn, login, logout } = useAuth();
+
+    function checkToken() {
+        const token = getJwt();
+        if (token) {
+            login();
+        }
+    }
+
+    checkToken();
+
     return <>
         <h2>Your payment has not been processed. 
             Please try again or contact admin
--- src/SuccessPayment.jsx
+++ src/SuccessPayment.jsx
@@ -1,6 +1,20 @@
 
+import { useAuth } from './AuthContext';
+import { useJwt } from "./UserStore";
 
 export default function SuccessPayment() {
+    const { getJwt } = useJwt();
+    const { isLoggedIn, login, logout } = useAuth();
+
+    function checkToken() {
+        const token = getJwt();
+        if (token) {
+            login();
+        }
+    }
+
+    checkToken();
+
     return <>
         <h2>Your payment has been processed</h2>
     </>
